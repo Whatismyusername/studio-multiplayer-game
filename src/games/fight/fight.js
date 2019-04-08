6434c0d5ea0e3b3b93b7271ded7562c6e78f2b8e
Whatismyusername <linhong.zhou@mihsnyc.org>
2019-04-08 18:30:29 -0400

    4/8/19

--- src/games/fight/fight.js
+++ src/games/fight/fight.js
@@ -3,10 +3,21 @@ import React from "react";
 import UserApi from "../../UserApi.js";
 
 export default class fight extends GameComponent {
+  constructor(props) {
+    super(props);
+    this.getSessionDatabaseRef().set({
+      text: "Good bye, World!"
+    });
+  }
+
   render() {
+    //Identify the Users
     var id = this.getSessionId();
     var users = this.getSessionUserIds().map(user_id => (
-      <li key={user_id}>{UserApi.getName(user_id)}</li>
+      <li key={user_id}>
+        <img key={user_id} src={UserApi.getPhotoUrl(user_id)} height="50px" />
+        {UserApi.getName(user_id)}
+      </li>
     ));
     var creator = UserApi.getName(this.getSessionCreatorUserId());
     var identity;
@@ -15,12 +26,14 @@ export default class fight extends GameComponent {
     } else {
       identity = "guest";
     }
+
+    this.getSessionDatabaseRef().set({ text: "Hello, World!" });
     return (
       <div>
         <p>Session ID: {id}</p>
         <p>Session creator: {creator}</p>
         <p>Session users:</p>
-        <ul> {users} </ul>
+        <ul>{users} </ul>
         <p>I am the {identity}!</p>
       </div>
     );
